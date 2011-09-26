2d7cdd6fe2c30b63d1f87426f2e4daa790f7f22d
Tonu Mikk <tmikk@umn.edu>
2011-09-26 15:31:51 -0500

    Added files from exercise 13

---
+++ ex13_fewer.py
@@ -0,0 +1,8 @@
+from sys import argv
+
+script, first, second = argv
+
+print "The script is called: ", script
+print "Your first variable is: ", first
+print "your second variable is: ", second
+
---
+++ ex13_input.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+print "The script is called: ", script
+print "It knows how tall you are: ", first
+print "It knows how old you are: ", second
+print "It knows how much you weigh: ", third
+print "but it doesn't know your name", raw_input ("Type your name: ")
