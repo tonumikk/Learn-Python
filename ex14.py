3b3a6255cab4baa807fc4d68c6a518a7ebf494a4
Tonu Mikk <tmikk@umn.edu>
2011-09-29 15:43:33 -0500

    Changed the prompt to >>>

--- ex14.py
+++ ex14.py
@@ -1,7 +1,7 @@
 from sys import argv
 
 script, user_name = argv
-prompt = '> '
+prompt = '>>> '
 
 print "Hi %s, I'm the %s script." % (user_name, script)
 print "I'd like to ask you a few questions."
