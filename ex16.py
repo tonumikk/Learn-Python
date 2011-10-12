545e394abf7d1565e4e4a68ae42384fbbd6f4032
Tonu Mikk <tmikk@umn.edu>
2011-10-12 13:08:17 -0500

    Writing target.write in a single line

--- ex16.py
+++ ex16.py
@@ -21,13 +21,8 @@ line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
-
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+ 
+target.write("%r \n %r \n %r \n" % (line1,line2,line3))  
 
 print "And finally, we close it."
 target.close()
