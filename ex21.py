0c78b9a1457415dd461d46b9c5750a4cc5bf3a7b
Tonu Mikk <tmikk@umn.edu>
2011-10-19 13:49:20 -0500

    Added the extra credit code for extra credit part 2 and 4.

--- ex21.py
+++ ex21.py
@@ -30,3 +30,17 @@ print "Here is a puzzle."
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it my hand?"
+
+# Figuring out the puzzle
+# First we add two values age and the other value
+
+divide1 = divide(iq, 2)
+multiply1 = multiply(weight, divide1)
+subtract1 = subtract(height, multiply1)
+what_new = add(age, subtract1)
+print "Let's see if this does it: ", what_new
+
+# Let's create a different formula ... Let's make it height divided by age plus weight
+new_what = add(divide(height, age), weight)
+
+print "Let's now print the new formula: ", new_what
