095711cf421376b08e78e391295683cb55187798
Tonu Mikk <tmikk@umn.edu>
2011-12-09 13:03:05 -0600

    Created a function for the while loop.

--- ex33.py
+++ ex33.py
@@ -1,15 +1,18 @@
-i = 0 
+#i = 0 
 numbers = []
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+def while_loop(i):
+    while i < 6:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-    i = i + 1
-    print "numbers now: ", numbers
-    print "At the bottom i is %d" % i
+        i = i + 1
+        print "numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-print "The numbers: "
+    print "The numbers: "
 
-for num in numbers:
-    print num
+    for num in numbers:
+        print num
+
+while_loop(0)
