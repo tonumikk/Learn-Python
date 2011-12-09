0413fa7c54e4bd2130975e1d26cedff5f8267aaa
Tonu Mikk <tmikk@umn.edu>
2011-12-09 13:42:07 -0600

    Added the for loop with numbers2 list.  Created a pause for by prompting for keyboard Enter key.

--- ex33.py
+++ ex33.py
@@ -1,7 +1,7 @@
-# i = 0 
-numbers = []
+print "First the while loop..."
 
 def while_loop(i, some_number, incrementor):
+    numbers = []
     while i < some_number:
         print "At the top i is %d" % i
         numbers.append(i)
@@ -16,3 +16,24 @@ def while_loop(i, some_number, incrementor):
         print num
 
 while_loop(0, 10, 2)
+
+print "_______________________________________"
+raw_input("Press Enter to continue...")
+print "And now the same with the for loop :-)."
+
+def for_loop(i, some_number):
+    numbers2 = []
+    incrementor = 2
+    for i in range(i, some_number, incrementor):
+        print "At the top i is %d" % i
+        numbers2.append(i)
+
+        print "numbers now: ", numbers2
+        print "At the bottom i is %d" % i
+
+    print "The numbers: "
+
+    for num in numbers2:
+        print num
+
+for_loop(0, 10)
