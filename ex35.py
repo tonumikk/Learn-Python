75779aa2f58b98abc01b31afc2d1cc1fbb8af8ce
Tonu Mikk <tmikk@umn.edu>
2011-12-12 16:19:45 -0600

    Improved how it checks if input is a number.

--- ex35.py
+++ ex35.py
@@ -4,7 +4,8 @@ def gold_room():
     print "This room is full of gold. How much do you take?"
 
     next = raw_input("> ")
-    if "0" in next or "1" in next:
+    if str.isdigit(next):
+    # if "0" in next or "1" in next:
         how_much = int(next)
     else:
         dead("Man, learn to type a number.")
